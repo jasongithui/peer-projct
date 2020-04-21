bb0b7339ddfdd28d5bea7b4a6c7fe9c888d4e8e3
Jason Githui <jasongithui@gmail.comgit config --global user.name jasongithuiomit config --globalgit user.mail jasongithui@gmail.com>
2020-04-21 11:59:50 +0300

    add top header words

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 //Get the button
-var mybutton = document.getElementById("scrollTop");
+var myButton = document.getElementById("scrollTop");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    mybutton.style.display = "block";
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
 }
 
