a0ba923a32dff1dc446226ec9ae16b8d7fa61470
Jason Githui <jasongithui@gmail.comgit config --global user.name jasongithuiomit config --globalgit user.mail jasongithui@gmail.com>
2020-04-21 11:08:41 +0300

    add scrollup button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //Get the button
-var mybutton = document.getElementById("sortDown");
+var mybutton = document.getElementById("scrollTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
